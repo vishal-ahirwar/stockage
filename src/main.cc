2ef8844d65f791251aea18a5b3fab56a7360a668
Vishal Ahirwar <vishalchoudhry0091@gmail.com>
2024-11-27 20:27:33 +0530

    init

---
+++ src/main.cc
@@ -0,0 +1,10 @@
+
+//Auto Genrated C++ file by ccli CLI
+//Copyright 2023 Vishal Ahirwar //replace it with your copyright notice!
+#include<iostream>
+int main(int argc,char*argv[])
+{
+    std::cout<<"@Hello, stockage\nhappy coding journey :)\n";
+    return 0;
+};
+
